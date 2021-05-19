cc2d536ced5337cc89d82c97d0704151682114bc
abho11 <royarunabh121@gmail.com>
2021-05-19 11:49:01 +0530

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn maskdet.wsgi --log-file -
---
+++ main/views.py
@@ -0,0 +1,27 @@
+from datetime import date
+from django.shortcuts import render
+import pyrebase
+
+
+
+config = {
+    "apiKey": "AIzaSyDApLG9x8dCakpoyvzNDZc0kCx2lui1wgg",
+    "authDomain": "test-3f380.firebaseapp.com",
+    "databaseURL": "https://test-3f380-default-rtdb.firebaseio.com",
+    "projectId": "test-3f380",
+    "storageBucket": "test-3f380.appspot.com",
+    "messagingSenderId": "1094542591459",
+    "appId": "1:1094542591459:web:9be05b403f91caa2428cd6",
+    "measurementId": "G-BVXMLJGGYH"
+}
+
+firebase = pyrebase.initialize_app(config)
+database=firebase.database()
+
+
+def home(request):
+    n = database.child('name').get().val()
+    dt = database.child('date').get().val()
+    return render(request,"home.html",{"n":n,"dt":dt })
+
+    
