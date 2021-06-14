3d5a50644bbd4a1dfbfe967c05f1d8ba1f97e756
abho11 <royarunabh121@gmail.com>
2021-06-14 11:03:54 +0530

    14-6-21

--- dj_firebase/settings.py
+++ dj_firebase/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '!lkxhb+59!5e9+@hrz(l72hob9o6%prq61)h0=6#439y*bcbd7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['maskhost.herokuapp.com' , '127.0.0.1']
+ALLOWED_HOSTS = ['mask-detection-alert.herokuapp.com' , '127.0.0.1']
 
 
 # Application definition
