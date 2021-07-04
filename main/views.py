de7da9cf38be142a88b25b68b3a6d608e352a19c
abho11 <royarunabh121@gmail.com>
2021-07-05 01:48:25 +0530

    camera

--- main/views.py
+++ main/views.py
@@ -10,6 +10,8 @@ from django.http import StreamingHttpResponse
 import threading
 import cv2
 from time import sleep
+import logging as log
+
 
 cred=credentials.Certificate("finalyearnmit-firebase-adminsdk-sk5ac-a5da39de07.json")
 firebase_admin.initialize_app(cred,{'databaseURL':'https://finalyearnmit-default-rtdb.firebaseio.com/'})
@@ -30,6 +32,50 @@ def home(request):
  return render(request,'home.html')
 
 def capture(request):
+   
+   
+   log.basicConfig(filename='webcam.log',level=log.INFO)
+
+   video_capture = cv2.VideoCapture(0)
+   
+
+   while True:
+      if not video_capture.isOpened():
+         print('Unable to load camera.')
+         sleep(5)
+         pass
+
+      # Capture frame-by-frame
+      ret, frame = video_capture.read()
+
+      # Display the resulting frame
+      cv2.imshow('Video', frame)
+
+      if cv2.waitKey(1) & 0xFF == ord('s'): 
+
+         check, frame = video_capture.read()
+         cv2.imshow("Capturing", frame)
+         cv2.imwrite(filename='./main/static/saved_img.jpg', img=frame)
+         video_capture.release()
+      
+         cv2.destroyAllWindows()
+
+         return render(request,'home.html')
+      elif cv2.waitKey(1) & 0xFF == ord('q'):
+         print("Turning off camera.")
+         video_capture.release()
+         print("Camera off.")
+         print("Program ended.")
+         cv2.destroyAllWindows()
+         return render(request,'home.html')
+
+      # Display the resulting frame
+      cv2.imshow('Video', frame)
+
+   # When everything is done, release the capture
+   video_capture.release()
+   cv2.destroyAllWindows()
+'''   
    key = cv2. waitKey(1)
    webcam = cv2.VideoCapture(0)
    sleep(2)
@@ -38,7 +84,7 @@ def capture(request):
          check, frame = webcam.read()
          print(check) #prints true as long as the webcam is running
          print(frame) #prints matrix values of each framecd 
-         cv2.imshow("Capturing", frame)
+         cv2.imshow("/main/static/Capturing", frame)
          key = cv2.waitKey(1)
          if key == ord('s'): 
                cv2.imwrite(filename="/main/static/saved_img.jpg", img=frame)
@@ -58,3 +104,4 @@ def capture(request):
          cv2.destroyAllWindows()
          return render(request,'home.html')
           
+'''
